e55d265e887312c5fa7368ccae6790f027a58c23
Preseka Zsolt <zsolt.preseka@gmail.com>
2020-07-14 20:53:22 +0200

    content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContentComponent } from './components/content/content.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 const routes: Routes = [
-  {path: '', component: ContentComponent}
+  {path: '', component: HomeComponent}
 ];
 
 @NgModule({
