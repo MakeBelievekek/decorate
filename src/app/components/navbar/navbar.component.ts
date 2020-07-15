ae33883f0292c231e6a7abb5b90a4f083fef6ff8
TOM <matetothuk@gamil.com>
2020-07-15 17:14:15 +0200

    navbar ok on full screen

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  screenHeight: number;
+  screenWidth: number;
+  show: boolean;
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+    this.getScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  getScreenSize(event?) {
+    this.screenHeight = window.innerHeight;
+    this.screenWidth = window.innerWidth;
+    console.log(this.screenHeight, this.screenWidth);
+    if (this.screenWidth >= 800) {
+      this.show = true;
+    } else {
+      this.show = false;
+    }
   }
 
 }
