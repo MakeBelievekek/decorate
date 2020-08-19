a611c3a267251b935dd9efd695252a4e6785ca47
WhiteSept <zsolt.preseka@gmail.com>
2020-08-19 10:46:21 +0200

    local

--- src/app/components/admin/admin-product-form/admin-product-form.component.ts
+++ src/app/components/admin/admin-product-form/admin-product-form.component.ts
@@ -1,92 +1,99 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
-import { AttributeData } from '../../../models/attributeData';
-import { AttributeListItemModel } from '../../../models/attributeListItemModel';
-import { FormDataModel } from '../../../models/formDataModel';
-import { ImageModel } from '../../../models/imageModel';
-import { WallpaperModel } from '../../../models/wallpaper-model';
-import { AdminService } from '../../../services/admin.service';
+import {HttpClient} from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormControl, FormGroup} from '@angular/forms';
+import {AttributeData} from '../../../models/attributeData';
+import {AttributeListItemModel} from '../../../models/attributeListItemModel';
+import {FormDataModel} from '../../../models/formDataModel';
+import {ImageModel} from '../../../models/imageModel';
+import {WallpaperModel} from '../../../models/wallpaper-model';
+import {AdminService} from '../../../services/admin.service';
 
 
 @Component({
-    selector: 'app-admin-product-form',
-    templateUrl: './admin-product-form.component.html',
-    styleUrls: ['./admin-product-form.component.css'],
+  selector: 'app-admin-product-form',
+  templateUrl: './admin-product-form.component.html',
+  styleUrls: ['./admin-product-form.component.css'],
 })
 export class AdminProductFormComponent implements OnInit {
 
-    productForm: FormGroup;
-    attributeList: FormDataModel = new class implements FormDataModel {
-        attributes: AttributeData[];
+  productForm: FormGroup;
+  attributeList: FormDataModel = new class implements FormDataModel {
+    attributes: AttributeData[];
+  };
+  colors: AttributeData[] = [];
+  patterns: AttributeData[] = [];
+  styles: AttributeData[] = [];
+  types: string[] = ['Wallpaper', 'Curtain']
+
+  constructor(private adminService: AdminService, private http: HttpClient) {
+    this.productForm = new FormGroup({
+      'productType': new FormControl(''),
+      'productName': new FormControl(''),
+      'productDesc': new FormControl(''),
+      'productItemNumber': new FormControl(''),
+      'productWidth': new FormControl(''),
+      'productPatternRep': new FormControl(''),
+      'productPrice': new FormControl(''),
+      'productComposition': new FormControl(''),
+      'productColors': new FormArray([]),
+      'productStyles': new FormArray([]),
+      'productPatterns': new FormArray([]),
+      'primaryImg': new FormControl(''),
+      'secondaryImg': new FormControl(''),
+      'productFamily': new FormControl(''),
+      'productAnnotation': new FormControl(''),
+      'productGlue': new FormControl(''),
+    });
+  }
+
+  ngOnInit(): void {
+    this.adminService.getAllAttribute().subscribe((data) => {
+      this.attributeList.attributes = data.attributes;
+    }, () => {
+    }, () => {
+      for (let attr of this.attributeList.attributes) {
+        if (attr.type === 'Color') {
+          this.colors.push(attr);
+        }
+        if (attr.type === 'Pattern') {
+          this.patterns.push(attr);
+        }
+        if (attr.type === 'Style') {
+          this.styles.push(attr);
+        }
+      }
+      this.adminService.createCheckboxControls(this.colors, this.patterns, this.styles, this.productForm)
+    });
+  }
+
+  saveProduct() {
+    const data = {...this.productForm.value};
+    data.productColors = this.adminService.createColorsArrayToSend(this.productForm, this.colors);
+    data.productPatterns = this.adminService.createPatternsArrayToSend(this.productForm, this.patterns);
+    data.productStyles = this.adminService.createStylesArrayToSend(this.productForm, this.styles);
+    console.log(data);
+  }
+
+  getValuesFromForm(): WallpaperModel {
+    const data: WallpaperModel = new class implements WallpaperModel {
+      annotation: string;
+      attributes: AttributeListItemModel[];
+      composition: string;
+      images: ImageModel[];
+      itemNumber: number;
+      name: string;
+      patternRep: number;
+      price: number;
+      productDesc: string;
+      productFamily: string;
+      productType: string;
+      recommendedGlue: string;
+      width: number;
     };
-    colors: AttributeData[] = [];
-    patterns: AttributeData[] = [];
-    styles: AttributeData[] = [];
+    data.name = this.productForm.controls['productName'].value;
+    data.productDesc = this.productForm.controls['productDesc'].value;
 
-    constructor(private adminService: AdminService, private http: HttpClient) {
-        this.productForm = new FormGroup({
-            'productName': new FormControl(''),
-            'productDesc': new FormControl(null),
-            'productPatterns': new FormControl(''),
-            'productColors': new FormArray([]),
-            'primaryImg': new FormControl([]),
-            'secondaryImg': new FormControl([]),
-        });
-    }
-
-    ngOnInit(): void {
-        this.adminService.getAllAttribute().subscribe((data) => {
-            this.attributeList.attributes = data.attributes;
-        }, () => {}, () => {
-            for (let attr of this.attributeList.attributes) {
-                if (attr.type === 'Color') {
-                    this.colors.push(attr);
-                }
-                if (attr.type === 'Patter') {
-                    this.patterns.push(attr);
-                }
-                if (attr.type === 'Style') {
-                    this.styles.push(attr);
-                }
-            }
-            this.createCheckboxControls(); });
-
-    }
-
-    private createCheckboxControls() {
-        this.colors.forEach(() => {
-            const control = new FormControl(false);
-            (this.productForm.controls.productColors as FormArray).push(control);
-        });
-    }
-
-    saveProduct() {
-        const data = {...this.productForm.value};
-        console.log(data);
-        this.adminService.createProduct(this.getValuesFromForm()).subscribe(() => {});
-    }
-
-    getValuesFromForm(): WallpaperModel {
-        const data: WallpaperModel = new class implements WallpaperModel {
-            annotation: string;
-            attributes: AttributeListItemModel[];
-            composition: string;
-            images: ImageModel[];
-            itemNumber: number;
-            name: string;
-            patternRep: number;
-            price: number;
-            productDesc: string;
-            productFamily: string;
-            productType: string;
-            recommendedGlue: string;
-            width: number;
-        };
-        data.name = this.productForm.controls['productName'].value;
-        data.productDesc = this.productForm.controls['productDesc'].value;
-
-        return data;
-    }
+    return data;
+  }
 
 }
--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -1,25 +1,58 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
-import { FormDataModel } from '../models/formDataModel';
-import { WallpaperModel } from '../models/wallpaper-model';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable, Subject} from 'rxjs';
+import {FormDataModel} from '../models/formDataModel';
+import {WallpaperModel} from '../models/wallpaper-model';
+import {FormArray, FormBuilder, FormControl, FormGroup} from "@angular/forms";
+import {AttributeListItemModel} from "../models/attributeListItemModel";
+import {AttributeData} from "../models/attributeData";
 
 const BASE_URL = 'https://localhost:8443';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class AdminService {
 
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
-    toggleService = new Subject<boolean>();
+  toggleService = new Subject<boolean>();
 
-    createProduct(data: WallpaperModel): Observable<WallpaperModel> {
-        return this.http.post<WallpaperModel>(BASE_URL + '/admin', data);
-    }
+  createProduct(data: WallpaperModel): Observable<WallpaperModel> {
+    return this.http.post<WallpaperModel>(BASE_URL + '/admin', data);
+  }
 
-    getAllAttribute(): Observable<FormDataModel> {
-        return this.http.get<FormDataModel>(BASE_URL + '/admin/formData');
-    }
+  getAllAttribute(): Observable<FormDataModel> {
+    return this.http.get<FormDataModel>(BASE_URL + '/admin/formData');
+  }
+
+  createCheckboxControls(colors: AttributeData[], patterns: AttributeData[], styles: AttributeData[], form: FormGroup) {
+    colors.forEach(() => {
+      const control = new FormControl(false);
+      (form.controls.productColors as FormArray).push(control);
+    });
+    styles.forEach(() => {
+      const control = new FormControl(false);
+      (form.controls.productStyles as FormArray).push(control);
+    });
+    patterns.forEach(() => {
+      const control = new FormControl(false);
+      (form.controls.productPatterns as FormArray).push(control);
+    });
+  }
+  createColorsArrayToSend(form:FormGroup,colors:AttributeData[]): string[] {
+    return form.value.productColors
+      .map((color, index) => color ? colors[index] : null)
+      .filter(color => color !== null);
+  }createPatternsArrayToSend(form:FormGroup,patterns:AttributeData[]): string[] {
+    return form.value.productPatterns
+      .map((pattern, index) => pattern ? patterns[index] : null)
+      .filter(pattern => pattern !== null);
+  }
+  createStylesArrayToSend(form:FormGroup,styles:AttributeData[]): string[] {
+    return form.value.productStyles
+      .map((style, index) => style ? styles[index] : null)
+      .filter(style => style !== null);
+  }
 }
