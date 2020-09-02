e68235dfb29f8840386eb6eeb0907672c113a8ff
TOM <matetothuk@gamil.com>
2020-09-02 23:26:09 +0200

    active filter large screen rdy

--- src/app/components/products/active-filters/active-filters.component.ts
+++ src/app/components/products/active-filters/active-filters.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
 import {FilterService} from '../../../services/filter.service';
 import {FilterControlModel} from '../../../models/filterControl.model';
 
@@ -7,14 +7,35 @@ import {FilterControlModel} from '../../../models/filterControl.model';
   templateUrl: './active-filters.component.html',
   styleUrls: ['./active-filters.component.css']
 })
-export class ActiveFiltersComponent implements OnInit {
+export class ActiveFiltersComponent implements OnInit, OnChanges {
   filterControl: FilterControlModel;
+  activeFilterExists: boolean;
+  @ViewChild('activeColorFilter') activeColorFilter: ElementRef;
 
-  constructor(private filerService: FilterService) {
+  constructor(private filterService: FilterService) {
   }
 
   ngOnInit(): void {
-    this.filterControl = this.filerService.filterControl;
+    this.filterControl = this.filterService.filterControl;
+    this.setActiveFilterExists();
   }
 
+  ngOnChanges(changes: any) {
+    console.log('change');
+    this.setActiveFilterExists();
+  }
+
+  handleActiveColors(color: string): void {
+    this.filterService.handleActiveColors(color);
+    this.setActiveFilterExists();
+  }
+
+  setActiveFilterExists() {
+    console.log(this.filterControl.activeColors.length);
+    if (this.filterControl.activeColors.length < 1) {
+      this.activeFilterExists = false;
+    } else {
+      this.activeFilterExists = true;
+    }
+  }
 }
