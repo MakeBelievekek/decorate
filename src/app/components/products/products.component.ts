e78bfd3ea24c714ecbd70d5c706b7bac8d87c82c
TOM <matetothuk@gamil.com>
2020-09-08 11:59:17 +0200

    lists

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,5 +1,6 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ScreenService} from '../../services/screen.service';
+import {ScreenControlModel} from '../../models/screenControl.model';
 
 @Component({
   selector: 'app-products',
@@ -9,11 +10,13 @@ import {ScreenService} from '../../services/screen.service';
 export class ProductsComponent implements OnInit {
   @ViewChild('productContainer') productContainer: ElementRef;
   @ViewChild('productContent') productContent: ElementRef;
+  screenControl: ScreenControlModel;
 
   constructor(private screenService: ScreenService) {
   }
 
   ngOnInit(): void {
+    this.screenControl = this.screenService.screenControl;
   }
 
   setScreenAttributes() {
