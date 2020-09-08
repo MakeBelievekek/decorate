29ef26bd5a75b943dec11371f332a731f882d885
Preseka Zsolt <zsolt.preseka@gmail.com>
2020-09-08 08:20:41 +0200

    checkout

--- src/app/components/home/content/content.component.ts
+++ src/app/components/home/content/content.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-content',
-  templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+    selector: 'app-content',
+    templateUrl: './content.component.html',
+    styleUrls: ['./content.component.css'],
 })
 export class ContentComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+
+    @Input() darkener: string ='https://photos.google.com/album/AF1QipN86tUsUseHIsPR6sxvllwgHQmIOPIL8hXjjSDV/photo/AF1QipNOH-yDnFRQDdp-Iqbqqoq4_yylHcUkyFtOEwCI';
+    color:string = 'red';
+
+    ngOnInit(): void {
+        console.log(this.darkener);
+    }
 
 }
---
+++ src/app/models/homeModel.ts
@@ -0,0 +1,4 @@
+export interface HomeModel {
+    imgUrl: string;
+    type: string;
+}
