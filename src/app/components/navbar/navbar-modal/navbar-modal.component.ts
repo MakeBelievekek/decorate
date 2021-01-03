14e2c65b765fc766372c3244b4673456ffa349ae
Preseka Zsolt <zsolt.preseka@gmail.com>
2021-01-03 17:37:58 +0100

    bread

--- src/app/components/navbar/navbar-modal/navbar-modal.component.ts
+++ src/app/components/navbar/navbar-modal/navbar-modal.component.ts
@@ -1,146 +1,132 @@
-import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
-import {AttributeModel} from '../../../models/attributeModel';
-import {FilterModel} from '../../../models/filterModel';
-import {ModalControllerModel} from '../../../models/modalController.model';
-import {ProductCategoryModalModel} from '../../../models/ProductCategoryModalModel';
-import {ModalService} from '../../../services/modal.service';
-import {RegexService} from '../../../services/regexService';
-import {SearchModel} from '../../../models/searchModel';
-import {ActiveFilterService} from '../../../services/active-filter.service';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AttributeModel } from '../../../models/attributeModel';
+import { FilterModel } from '../../../models/filterModel';
+import { ModalControllerModel } from '../../../models/modalController.model';
+import { ProductCategoryModalModel } from '../../../models/ProductCategoryModalModel';
+import { SearchModel } from '../../../models/searchModel';
+import { ActiveFilterService } from '../../../services/active-filter.service';
+import { ModalService } from '../../../services/modal.service';
+import { RegexService } from '../../../services/regexService';
 
 const curtainPath = '/fuggony/';
 const otherPath = '';
 
 @Component({
-  selector: 'app-navbar-modal',
-  templateUrl: './navbar-modal.component.html',
-  styleUrls: ['./navbar-modal.component.css'],
+    selector: 'app-navbar-modal',
+    templateUrl: './navbar-modal.component.html',
+    styleUrls: ['./navbar-modal.component.css'],
 })
 export class NavbarModalComponent implements OnInit, OnDestroy {
 
-  @Input() products: ProductCategoryModalModel[] = [];
+    @Input() products: ProductCategoryModalModel[] = [];
 
-  actualProduct = new ProductCategoryModalModel();
-  filter = new FilterModel();
-  modalControl: ModalControllerModel;
-  @Input() isShowing: boolean;
-  @Input() animationState: string;
-  @Output() productModalCloseAnimFinished = new EventEmitter<boolean>();
-  margin = '0px';
-  path = '';
-  params: any;
+    actualProduct = new ProductCategoryModalModel();
+    filter = new FilterModel();
+    modalControl: ModalControllerModel;
+    @Input() isShowing: boolean;
+    @Input() animationState: string;
+    @Output() productModalCloseAnimFinished = new EventEmitter<boolean>();
+    margin = '0px';
+    path = '';
+    params: any;
 
-  constructor(private modalService: ModalService,
-              private router: Router,
-              private activatedRoute: ActivatedRoute,
-              private regexService: RegexService,
-              private activeFilterService: ActiveFilterService,
-  ) {
+    constructor(private modalService: ModalService,
+                private router: Router,
+                private activatedRoute: ActivatedRoute,
+                private regexService: RegexService,
+                private activeFilterService: ActiveFilterService,
+    ) {
 
-  }
-
-  ngOnInit(): void {
-    this.actualProduct = this.products[0];
-    this.modalControl = this.modalService.modalControl;
-  }
-
-  keepModalOnScreen() {
-    this.closeNavigationModal();
-  }
-//eltűntek a dolgaim
-
-  visible(product: ProductCategoryModalModel) {
-    this.products.forEach(value => {
-      value.isShown = false;
-      value.color = false;
-      value.pattern = false;
-      value.style = false;
-    });
-    product.isShown = !product.isShown;
-    this.actualProduct = product;
-    switch (this.actualProduct) {
-      case this.products[0]: {
-        this.margin = '0px';
-        break;
-      }
-      case this.products[1]: {
-        this.margin = '64px';
-        break;
-      }
-      case this.products[2]: {
-        this.margin = '128px';
-        break;
-      }
-      case this.products[3]: {
-        this.margin = '192px';
-        break;
-      }
     }
-  }
 
-  attrVisible(attr: string) {
-    switch (attr) {
-      case 'color': {
-        this.actualProduct.color = true;
-        this.actualProduct.pattern = false;
-        this.actualProduct.style = false;
-        break;
-      }
-      case 'pattern': {
-        this.actualProduct.color = false;
-        this.actualProduct.pattern = true;
-        this.actualProduct.style = false;
-        break;
-      }
-      case 'style': {
-        this.actualProduct.color = false;
-        this.actualProduct.pattern = false;
-        this.actualProduct.style = true;
-        break;
-      }
+    ngOnInit(): void {
+        this.actualProduct = this.products[0];
+        this.modalControl = this.modalService.modalControl;
     }
-  }
 
-  closeNavigationModal() {
-    this.isShowing = false;
-    this.productModalCloseAnimFinished.emit(true);
-  }
-
-  buildRoute(p: ProductCategoryModalModel): Array<string> {
-    return p.productDatabaseName === null ?
-      ['termekek', 'fuggony', this.regexService.urlWithoutAccents(p.productType)] :
-      ['termekek', this.regexService.urlWithoutAccents(p.productType)];
-  }
-
-  generateParams(searchModel: SearchModel): any {
-    if (searchModel) {
-      const {subType, ...rest} = searchModel;
-      return rest;
+    keepModalOnScreen() {
+        this.closeNavigationModal();
     }
-  }
 
-  generateParamsWhitAttribute(actualProduct: ProductCategoryModalModel, attribute: AttributeModel): any {
-    const searchModel = {...actualProduct.searchModel};
-    searchModel.attributeIds = [attribute.id];
 
-    return this.generateParams(searchModel);
-  }
+    visible(product: ProductCategoryModalModel) {
+        this.products.forEach(value => {
+            value.isShown = false;
+            value.color = false;
+            value.pattern = false;
+            value.style = false;
+        });
+        product.isShown = !product.isShown;
+        this.actualProduct = product;
+        let actual = this.products.indexOf(this.actualProduct);
+        let max = this.products.length - 3;
+        this.margin = actual > max ? max * 64 + 'px' : actual * 64 + 'px';
 
-  navigateWhitAttribute(actualProduct: ProductCategoryModalModel, attribute: AttributeModel) {
-    const searchModelForQueryParams = this.generateParamsWhitAttribute(actualProduct, attribute);
-    const route = this.buildRoute(actualProduct);
+    }
 
-    this.router.navigate(route, {queryParams: {...searchModelForQueryParams}});
-  }
+    attrVisible(attr: string) {
+        switch (attr) {
+            case 'color': {
+                this.actualProduct.color = true;
+                this.actualProduct.pattern = false;
+                this.actualProduct.style = false;
+                break;
+            }
+            case 'pattern': {
+                this.actualProduct.color = false;
+                this.actualProduct.pattern = true;
+                this.actualProduct.style = false;
+                break;
+            }
+            case 'style': {
+                this.actualProduct.color = false;
+                this.actualProduct.pattern = false;
+                this.actualProduct.style = true;
+                break;
+            }
+        }
+    }
 
-  navigateWhitMainType(product: ProductCategoryModalModel) {
-    const searchModelForQueryParams = this.generateParams(product.searchModel);
-    const route = this.buildRoute(product);
+    closeNavigationModal() {
+        this.isShowing = false;
+        this.productModalCloseAnimFinished.emit(true);
+    }
 
-    this.router.navigate(route, {queryParams: {...searchModelForQueryParams}});
-  }
+    buildRoute(p: ProductCategoryModalModel): Array<string> {
+        return p.productDatabaseName === null ?
+            ['termekek', 'fuggony', this.regexService.urlWithoutAccents(p.productType)] :
+            ['termekek', this.regexService.urlWithoutAccents(p.productType)];
+    }
 
-  ngOnDestroy(): void {
-  }
+    generateParams(searchModel: SearchModel): any {
+        if (searchModel) {
+            const {subType, ...rest} = searchModel;
+            return rest;
+        }
+    }
+
+    generateParamsWhitAttribute(actualProduct: ProductCategoryModalModel, attribute: AttributeModel): any {
+        const searchModel = {...actualProduct.searchModel};
+        searchModel.attributeIds = [attribute.id];
+
+        return this.generateParams(searchModel);
+    }
+
+    navigateWhitAttribute(actualProduct: ProductCategoryModalModel, attribute: AttributeModel) {
+        const searchModelForQueryParams = this.generateParamsWhitAttribute(actualProduct, attribute);
+        const route = this.buildRoute(actualProduct);
+
+        this.router.navigate(route, {queryParams: {...searchModelForQueryParams}});
+    }
+
+    navigateWhitMainType(product: ProductCategoryModalModel) {
+        const searchModelForQueryParams = this.generateParams(product.searchModel);
+        const route = this.buildRoute(product);
+
+        this.router.navigate(route, {queryParams: {...searchModelForQueryParams}});
+    }
+
+    ngOnDestroy(): void {
+    }
 }
